a845f00b6e3efec69eefa61237eb1360937361cf
xruslanx123 <xruslanx123@gmail.com>
2016-12-28 09:11:20 +0200

    Update User.java

--- app/src/main/java/co/go_app/app/User.java
+++ app/src/main/java/co/go_app/app/User.java
@@ -11,7 +11,41 @@ public class User {
     private String uID;
     private String photoUrl;
     private String emailAddress;
-    private ArrayList<String> AcceptedChallenges;
-
+    private ArrayList<String> acceptedChallenges;
+    private ArrayList<String> createdChallenges;
+    
+    //setters
+    public void setUID(String uID){
+        this.uID = uID;
+    }
+    public void setPhotoUrl(String url){
+        this.photoUrl = url;
+    }
+    public void setEmailAddress(String address){
+        this.emailAddress = address;
+    }
+    public void setAcceptedChallenges(ArrayList<String> acceptedChallenges){
+        this.acceptedChallenges = acceptedChallenges;
+    }
+    public void setCreatedChallenges(ArrayList<String> createdChallenges){
+        this.createdChallenges = createdChallenges;
+    }
+    
+    //getters
+    public String getUID(){
+        return this.uID;
+    }
+    public String getPhotoUrl(){
+        return this.photoUrl;
+    }
+    public String getEmailAddress(){
+        return this.emailAddress;
+    }
+    public ArrayList<String> getAcceptedChallenges(){
+        return this.acceptedChallenges;
+    }
+    public ArrayList<String> getCreatedChallenges(){
+        return this.createdChallenges;
+    }
 
 }
